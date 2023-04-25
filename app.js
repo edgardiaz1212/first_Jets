faebfb307d9aee9fae8d45317eb69b1754b773c3
Edgar Diaz <ed35376@gmail.com>
2023-04-25 18:11:29 +0000

    test finished

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
+// declaramos una funcion con el mismo nombre "formEuroToDollar"
+const fromEuroToDollar = function(valueInEuro){
+    // convertimos el valor a dolares
+    let valueInDollar = valueInEuro * 1.2;
+    // retornamos el valor
+    return valueInDollar;}
+
 const sum = (a,b)=> {
     return a+b
 }
 console.log(sum(7,3))
 
-module.exports = {sum};
+ const fromDollarToYen =function (valueInDollar) {
+     let valueInYen = valueInDollar*127.9/1.2
+    return valueInYen
+ }
+
+ const fromYenToPound = function(valueInYen){
+     let valueInPound = valueInYen * 0.8 / 127.9
+     return valueInPound
+  }
+ module.exports = {sum, fromEuroToDollar, fromDollarToYen , fromYenToPound};
